b9dd7b6816d7fb3907c18a8fc6a271c47e354e3d
anachor <anachored@gmail.com>
2020-02-20 18:19:30 +0600

    Add PCF

---
+++ KnightMare's Code Library/Temp/Prime Counting Function.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+#define MAXN 500
+#define MAXM 100010
+#define MAXP 666666
+#define MAX 10000010
+#define ll long long int
+#define chkbit(ar, i) (((ar[(i) >> 6]) & (1 << (((i) >> 1) & 31))))
+#define setbit(ar, i) (((ar[(i) >> 6]) |= (1 << (((i) >> 1) & 31))))
+#define isprime(x) (( (x) && ((x)&1) && (!chkbit(ar, (x)))) || ((x) == 2))
+
+namespace pcf{
+    long long dp[MAXN][MAXM];
+    unsigned int ar[(MAX>>6)+5] = {0};
+    int len=0, primes[MAXP], counter[MAX];
+
+    void Sieve(){
+        setbit(ar,0), setbit(ar,1);
+        for (int i=3;(i*i)<MAX;i++,i++){
+            if(!chkbit(ar, i)){
+                int k=i<<1;
+                for(int j=(i*i);j<MAX;j+=k) setbit(ar,j);
+            }
+        }
+
+        for(int i=1;i<MAX;i++){
+            counter[i]=counter[i - 1];
+            if(isprime(i)) primes[len++]=i, counter[i]++;
+        }
+    }
+
+    void init(){
+        Sieve();
+        for(int n=0;n<MAXN;n++){
+            for(int m=0;m<MAXM;m++){
+                if(!n) dp[n][m]=m;
+                else dp[n][m]=dp[n-1][m]-dp[n-1][m/primes[n-1]];
+            }
+        }
+    }
+
+    ll phi(ll m,int n){
+        if(n==0) return m;
+        if(primes[n-1]>=m) return 1;
+        if(m<MAXM && n<MAXN) return dp[n][m];
+        return phi(m,n-1) - phi(m/primes[n-1],n-1);
+    }
+
+    ll Lehmer(long long m){
+        if(m<MAX) return counter[m];
+
+        ll w,res=0;
+        int i,a,s,c,x,y;
+        s=sqrt(0.9+m), y=c=cbrt(0.9+m);
+        a=counter[y], res=phi(m,a)+a-1;
+        for(i=a;primes[i]<=s;i++) res=res-Lehmer(m/primes[i])+Lehmer(primes[i])-1;
+        return res;
+    }
+}
+
+int main() {
+    pcf::init();
+    int t;
+    cin>>t;
+
+    while (t--) {
+        int n;
+        cin>>n;
+
+        int lo = 1, hi = 2e9;
+
+        while (lo < hi) {
+            int m = (0LL + lo + hi)/2;
+            int pp = pcf::Lehmer(m);
+            int cc = m - 1 - pp;
+
+            if (cc < n)     lo = m+1;
+            else            hi = m;
+        }
+        cout<<lo-1<<endl;
+    }
+}
