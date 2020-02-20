525d167cf17b5f6c338d40568f900a9e8dae4d05
anachor <anachored@gmail.com>
2020-02-20 19:04:04 +0600

    Add Anymod in fft

--- Compressed/Math/FFT.cpp
+++ Compressed/Math/FFT.cpp
@@ -48,4 +48,39 @@ void multiply(VI &P, VI &Q, VI &res) {
   res.resize(M);
   for( int i=0; i<M; i++)  res[i] = round(A[i].x);
 }
+///use long double in fft if RT >= 13
+const int RT = 15;   ///Upto M <= 4^RT
+vector<LL>anymod(vector<LL>&a,vector<LL>&b,LL mod){
+  init(max(a.size(),b.size()));
+  vector<node> al(M), ar(M), bl(M), br(M);
+  for (int i=0; i<a.size(); i++) {
+    LL k = a[i]%mod; al[i] = node(k >> RT, 0);
+    ar[i] = node(k & ((1<<RT)-1), 0);
+  }
+  for (int i=0; i<b.size(); i++) {
+    LL k = b[i]%mod; bl[i] = node(k >>RT, 0);
+    br[i] = node(k & ((1<<RT)-1), 0);
+  }
+  ftransform(al, 0); ftransform(ar, 0);
+  ftransform(bl, 0); ftransform(br, 0);
+  for (int i=0; i<M; i++) {
+    node ll = al[i] * bl[i], lr = al[i] * br[i];
+    node rl = ar[i] * bl[i], rr = ar[i] * br[i];
+    al[i] = ll; ar[i] = lr;
+    bl[i] = rl; br[i] = rr;
+  }
+  ftransform(al, true); ftransform(ar, true);
+  ftransform(bl, true); ftransform(br, true);
+  vector<LL> ans(M);
+  for (int i=0; i<M; i++) {
+    LL right = round(br[i].x);
+    right %= mod;
+    LL mid=round(round(bl[i].x) + round(ar[i].x));
+    mid = ((mid%mod)<<RT)%mod;
+    LL left=round(al[i].x);
+    left = ((left%mod)<<(2*RT))%mod;
+    ans[i] = (left+mid+right)%mod;
+  }
+  return ans;
+}
 };
