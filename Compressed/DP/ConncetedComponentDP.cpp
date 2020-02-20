34ae4d5454745cede435dc3e18ade94fa479cbdc
Arghya115 <38067587+Arghya115@users.noreply.github.com>
2020-02-20 17:15:15 +0600

    Update ConncetedComponentDP.cpp

--- Compressed/DP/ConncetedComponentDP.cpp
+++ Compressed/DP/ConncetedComponentDP.cpp
@@ -17,15 +17,19 @@ i64 f(int n,int r,int k,int c,int st,int en)
     or be the first element or last */
   }
   else{
-    if(st==0) ans+=(f(n-1,r,k-1,c,1,en)+f(n-1,r,k-1,c-1,1,en)*c); // this is starting element
-    if(en==0) ans += ( f(n-1,r,k-1,c,st,1) + f( n-1,r,k-1,c-1,st,1)*c ) ; // this is ending element
+    if(st==0) ans+=(f(n-1,r,k-1,c,1,en)+f(n-1,r,
+    k-1,c-1,1,en)*c); // this is starting element
+    if(en==0) ans += ( f(n-1,r,k-1,c,st,1) +
+f(n-1,r,k-1,c-1,st,1)*c);// this is ending element
 
     ans += f( n-1 , r , k-1 , c+1 , st , en );
     // created & independent
-    ans += f(n-1,r,k-1,c,st,en)*2*c+f(n-1,r,k-1,c,st,en)*(st+en);
+    ans += f(n-1,r,k-1,c,st,en)*2*c+
+    f(n-1,r,k-1,c,st,en)*(st+en);
     /*created and connected with some other
     component possibly start or end component */
-    ans += (f(n-1,r,k-1,c-1,st,en)*c*(c-1)+f(n-1,r,k-1,c-1,st,en)*c*(st+en));
+    ans += (f(n-1,r,k-1,c-1,st,en)*c*(c-1)+
+            f(n-1,r,k-1,c-1,st,en)*c*(st+en));
 /*created and connected between two component */
   }
   return dp[n][r][k][c][st][en] = ans%mod ;
