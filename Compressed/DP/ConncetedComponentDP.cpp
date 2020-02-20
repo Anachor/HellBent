b734dfd07a465dff8d802e9ee067e5cc5d06d75a
Ashiq100 <55847757+Ashiq100@users.noreply.github.com>
2020-02-20 19:39:15 +0600

    Update ConncetedComponentDP.cpp

--- Compressed/DP/ConncetedComponentDP.cpp
+++ Compressed/DP/ConncetedComponentDP.cpp
@@ -1,18 +1,12 @@
-#include <bits/stdc++.h>
-using namespace std ;
-typedef pair<int, int> pi ;
-i64 dp[102][32][12][12][2][2] , M ;
-int a[102] ;
-i64 f(int n,int r,int k,int c,int st,int en)
-{
+i64 f(int n,int r,int k,int c,int st,int en) {
   if(n==0||c<0||st<0||en <0) return 0 ;
   r = (r+a[n]*(2*c+st+en))%M ;
-  if( dp[n][r][k][c][st][en]!=-1)
-    return dp[n][r][k][c][st][en] ;
-  i64 ans = f(n-1,r,k,c,st,en);/*itisnotused*/
-  if( k==1 ){
-    if( c==0 && (st||en) && r==0 ) ans++;/*if
-    this is the last element to take
+  if(dp[n][r][k][c][st][en]!=-1)
+    return dp[n][r][k][c][st][en];
+  i64 ans = f(n-1,r,k,c,st,en);/*it is not used*/
+  if(k==1) {
+    if( c==0 && (st||en) && r==0 ) ans++;
+    /* if this is the last element to take
     then is should either connect st and en ,
     or be the first element or last */
   }
@@ -21,7 +15,6 @@ i64 f(int n,int r,int k,int c,int st,int en)
     k-1,c-1,1,en)*c); // this is starting element
     if(en==0) ans += ( f(n-1,r,k-1,c,st,1) +
 f(n-1,r,k-1,c-1,st,1)*c);// this is ending element
-
     ans += f( n-1 , r , k-1 , c+1 , st , en );
     // created & independent
     ans += f(n-1,r,k-1,c,st,en)*2*c+
@@ -32,5 +25,5 @@ f(n-1,r,k-1,c-1,st,1)*c);// this is ending element
             f(n-1,r,k-1,c-1,st,en)*c*(st+en));
 /*created and connected between two component */
   }
-  return dp[n][r][k][c][st][en] = ans%mod ;
+  return dp[n][r][k][c][st][en] = ans%mod;
 }
