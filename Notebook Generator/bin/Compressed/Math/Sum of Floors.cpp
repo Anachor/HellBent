8d4e7c7162c62084fc37b0ec5e7e3fc7822252e3
anachor <anachored@gmail.com>
2020-02-20 19:44:26 +0600

    Del

--- Notebook Generator/bin/Compressed/Math/Sum of Floors.cpp
+++
@@ -1,7 +0,0 @@
-///Finds sum of floor((p*i+r)/q) from i = 0 to n-1
-LL findSum(LL n, LL p, LL r, LL q) {
-  if (p == 0) return (r / q) * n;
-  if (p>=q||r>=q) return findSum(n,p%q,r%q,q) +
-                        ((p/q)*(n-1)+2*(r/q))*n/2;
-  return findSum((p*n+r)/q, q, (p*n+r)%q,p);
-}
