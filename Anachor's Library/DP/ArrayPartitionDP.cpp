c0f1f458458caa18cd03cbda7a84526866c35842
anachor <anachored@gmail.com>
2021-04-08 14:55:48 +0600

    Optimize ArrayParttionDP and add problem name

--- Anachor's Library/DP/ArrayPartitionDP.cpp
+++ Anachor's Library/DP/ArrayPartitionDP.cpp
@@ -1,15 +1,35 @@
+/**
+Solves dp[n][k] = min(dp[i-1][k-1] + cost(i, n)) using aliens trick
+Req: cost() is QF. Complexity: O(n log^2)
+
+Possible optimizations for O(n log n)
+You can change solve1D() with linear CHT if cost(l, r) is of the form f(l)*g(r)
+Alternatively, it may be possible to remove the binary search in 1D-1D.
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long LL;
 typedef pair<LL, LL>  PLL;
 
 namespace ArrayPartitionDP {
-    PLL operator + (const PLL &a, const PLL &b) {
+    ///define this function in code
+    LL base_cost(int l, int r);
+
+    long long C;
+    int n;
+
+    PLL operator+ (const PLL &a, const PLL &b) {
         return PLL(a.first+b.first, a.second+b.second);
     }
 
+    PLL cost(int l, int r) {
+        return {base_cost(l, r)+C, 1};
+    }
+
     ///Solves dp[i] = min(dp[j] + cost(j+1, i)) given that cost() is QF
-    PLL solve1D(int n, PLL cost(int, int)) {
+    ///returns {dp[n], min no of partitions}
+    PLL solve1D() {
         vector<PLL> dp(n+1);
         vector<int> opt(n+1);
         deque<pair<int, int>> dq;
@@ -45,23 +65,13 @@ namespace ArrayPartitionDP {
         return dp[n];
     }
 
-    function<LL(int, int)> base_cost;
-    long long C;
-    int n;
-
-    PLL cost(int l, int r) {
-        return {base_cost(l, r)+C, 1};
-    }
-
     PLL check(long long c) {
         C = c;
-        return solve1D(n, cost);
+        return solve1D();
     }
 
-    long long solve(int N, int k, LL cost(int, int), long long lo, long long hi) {
-        base_cost = cost;
+    long long solve(int N, int k, long long lo, long long hi) {
         n = N;
-
         while (lo < hi) {
             long long mid = lo + (hi-lo)/2;
             if (check(mid).second > k)  lo = mid+1;
@@ -71,13 +81,15 @@ namespace ArrayPartitionDP {
     }
 }
 
-const int N = 1005;
-long long a[N], W[N], WA[N];
-long long ccost(int l,int r) {
-    return (W[r] - W[l-1])*a[r] - (WA[r] - WA[l-1]);
+///Solves https://tioj.ck.tp.edu.tw/problems/1986
+const int N = 3e5+7;
+long long a[N], A[N];
+
+LL ArrayPartitionDP::base_cost(int l, int r) {
+    int m = (l+r)/2;
+    long long Z = (A[r] - A[m]) - (A[m] - A[l-1]) + (l%2 == r%2 ? a[m]: 0);
+    return Z;
 }
-
-
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -85,11 +97,11 @@ int main() {
     int n, k;
 
     while (cin>>n>>k) {
-        for (int i=1; i<=n; i++)    {
-            cin>>a[i]>>W[i];
-            WA[i] = W[i] * a[i] + WA[i-1];
-            W[i] += W[i-1];
-        }
-        cout<<ArrayPartitionDP::solve(n, k, ccost, 0, 1e12)<<endl;
+        for (int i=1; i<=n; i++)    cin>>a[i];
+        sort(a+1, a+n+1);
+        for (int i=1; i<=n; i++)    A[i] = A[i-1] + a[i];
+
+        cout<<ArrayPartitionDP::solve(n, k, 0, 1e12)<<endl;
     }
+
 }
