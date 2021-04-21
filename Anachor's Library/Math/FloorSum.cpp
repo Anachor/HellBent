c9a1a02bac803a75e8925d2501658f3e74408b07
anachor <anachored@gmail.com>
2021-04-22 05:50:17 +0600

    Add FloorSum

---
+++ Anachor's Library/Math/FloorSum.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+LL mod(LL a, LL m) {
+    LL ans = a%m;
+    return ans < 0 ? ans+m : ans;
+}
+
+///Sum(floor((ax+b)/m)) for i=0 to n-1, (n,m >= 0)
+LL floorSum(LL n, LL m, LL a, LL b) {
+    LL ra = mod(a, m), rb = mod(b, m), k = (ra*n+rb);
+    LL ans = ((a-ra)/m) * n*(n-1)/2 + ((b-rb)/m) * n;
+    if (k < m)  return ans;
+    return ans + floorSum(k/m, ra, m, k%m);
+
+}
+
+///Solves https://judge.yosupo.jp/problem/sum_of_floor_of_linear
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin>>t;
+
+    while (t--) {
+        int n, m, a, b;
+        cin>>n>>m>>a>>b;
+        cout<<floorSum(n, m, a, b)<<"\n";
+    }
+}
