08827b4af270a80812273b536cd6495dd06abb3a
anachor <anachored@gmail.com>
2021-04-23 20:14:23 +0600

    ok

--- Anachor's Library/Data Structures/Fenwick.cpp
+++ Anachor's Library/Data Structures/Fenwick.cpp
@@ -1,35 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N = 1.1e6+7, K = 21;
-long long ft[N];
+struct Fenwick {
+    int N, K = 20;
+    vector<long long> ft;
+    Fenwick(int n) : N(n+1), ft(n+1) {}
 
-void add(int x, long long val) {
-    for (int idx=x; idx<N; idx += idx&-idx)
-        ft[idx] += val;
-}
-
-long long sum(int x) {
-    long long ans = 0;
-    for (int idx=x; idx>0; idx-=idx&-idx)
-        ans += ft[idx];
-    return ans;
-}
-
-///first k such that sum(k) >= x, if none returns N.
-int get(long long x) {
-    int ans = 0;
-    for (int i=K-1; i>=0; i--) {
-        int nxt = ans + (1<<i);
-        if (nxt < N && ft[nxt] < x) {
-            ans += 1<<i;
-            x-=ft[nxt];
-        }
+    void add(int x, long long val) {
+        for (int i=x; i<N; i+=i&-i) ft[i] += val;
     }
-    return ans+1;
-}
 
-/// Solves Timus 1896
+    long long sum(int x) {
+        long long ans = 0;
+        for (int i=x; i>0; i-=i&-i) ans += ft[i];
+        return ans;
+    }
+
+    ///first k such that sum(k) >= x, if none returns N.
+    int get(long long x) {
+        int ans = 0;
+        for (int i=K-1; i>=0; i--) {
+            int nxt = ans + (1<<i);
+            if (nxt < N && ft[nxt] < x) {
+                ans = nxt;
+                x -= ft[nxt];
+            }
+        }
+        return ans+1;
+    }
+};
+/// Solves Timus 1521
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -37,7 +37,9 @@ int main() {
     int n, k;
     cin>>n>>k;
 
-    for (int i=1; i<=n; i++)    add(i, 1);
+    Fenwick ft(n);
+
+    for (int i=1; i<=n; i++)    ft.add(i, 1);
 
     int last = 1;
     int ans = 0;
@@ -45,11 +47,9 @@ int main() {
         int cur = (last+k-1)%(n-i+1);
         if (cur == 0)   cur = (n-i+1);
 
-        int idx = get(cur);
-        ans ^= abs(idx-i);
-        add(idx, -1);
+        int idx = ft.get(cur);
+        cout<<idx<<" ";
+        ft.add(idx, -1);
         last = cur;
     }
-
-    cout<<ans<<endl;
 }
